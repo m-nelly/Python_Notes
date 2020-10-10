b1fe8d1f11b0512b5e0495c2d9797a3253218f17
netmn <netmn99@gmail.com>
2020-10-10 12:08:32 -0500

    Started Classes

--- Lessons/oop.py
+++ Lessons/oop.py
@@ -1,4 +1,18 @@
-def classes():
+def classes(): #working
+    # think of a class as a blueprint and an object as the constructed counterpart of that blueprint.
+    # the class can be used multiple times to build the same thing, but each iteration is a unique object. 
+
+    class FirstClass:
+
+        # __init__ is a special constructor method that performs actions during initialization.
+        def __init__(self, author, title): # the self attribute points this instance of the class.
+            self.author = author
+            self.title = title
+        
+        def printresult(self): # the self attribute allows instancing using objects.
+            print(self.author, " : ", self.title)
+
+
     return()
 
 def objects():
